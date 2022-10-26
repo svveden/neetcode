f2f449ab0ced7a014f45206920218a0c68d31723
Jared Moise <55561624+svveden@users.noreply.github.com>
2022-10-25 19:45:07 -0500

    Create contains_duplicate.cpp

---
+++ contains_duplicate.cpp
@@ -0,0 +1,16 @@
+//checks for duplicate in an array
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        vector<int> temp; //temp vect
+        for(int i = 0; i < nums.size(); i++){ //check each element in given vect
+            auto found = std::find(temp.begin(), temp.end(), nums[i]); //return iterator to duplicate or last element
+            if(found != end(temp)){ //if found != last element (iterator pointer to beyond vect) there is a dupe
+                return true;
+            } else{
+                temp.push_back(nums[i]); //else push current number onto temp vect
+            }
+        } return false;
+    }
+};
