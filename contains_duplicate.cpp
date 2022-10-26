65e58354da1423ee86f13d8dc067c8bae3df1d43
Jared Moise <55561624+svveden@users.noreply.github.com>
2022-10-25 19:45:39 -0500

    Update contains_duplicate.cpp

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -6,6 +6,7 @@ public:
         vector<int> temp; //temp vect
         for(int i = 0; i < nums.size(); i++){ //check each element in given vect
             auto found = std::find(temp.begin(), temp.end(), nums[i]); //return iterator to duplicate or last element
+            //https://cplusplus.com/reference/algorithm/find/ <- found std::find here 
             if(found != end(temp)){ //if found != last element (iterator pointer to beyond vect) there is a dupe
                 return true;
             } else{
